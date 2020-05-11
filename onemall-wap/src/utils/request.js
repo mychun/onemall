be07e5fd301af1fe5d3f38aa0aabe53676c28298
chunxhwan <chunxhwan@126.com>
2020-05-11 17:43:48 +0800

    delete

--- onemall-wap/src/utils/request.js
+++ onemall-wap/src/utils/request.js
@@ -4,7 +4,7 @@ import storage from 'good-storage';
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://192.168.1.102:8080/', // api 的 base_url
+  baseURL: 'http://192.168.1.197:8080/', // api 的 base_url
   timeout: 5000 // request timeout
 })
 
